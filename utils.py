afa61d55959d9a06ba02d4f69863f9bb77d999bd
Sanjana Garg <garg.sanjana.95@gmail.com>
2020-04-22 22:47:42 -0700

    Added code for generating bert embeddings

--- utils.py
+++ utils.py
@@ -41,8 +41,35 @@ def clean_twitter(text):
     text = re.sub(r'(#[A-Za-z0-9_]+)', ' ', text)  # remove hashtags
     text = re.sub(r'(\.){2,}', '.', text)
     text = re.sub(r"\s{2,}", " ", text)
+    text = re.sub(r"[^A-Za-z0-9(),.!?_\"\'\`]", " ", text)
     text = text.lower()
     return text
+
+def separate_punctuation(string):
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\"", " \" ", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'m", " \'m", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"\.", " . ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\$", " $ ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    return string.strip().lower()
+
+
+def clean_doc(data, is_tweet=False):
+    data = [s.strip() for s in data]
+    if is_tweet:
+        data = [clean_twitter(s) for s in data]
+    data = [separate_punctuation(s) for s in data]
+    return data
 
 
 def stem(sentence):
@@ -56,3 +83,19 @@ def preprocess(text):
     text = clean_twitter(text)
     text = stem(text)
     return text
+
+def pad_sequences(sentences, padding_word="<PAD/>", pad_len=None):
+    if pad_len is not None:
+        sequence_length = pad_len
+    else:
+        sequence_length = max(len(x) for x in sentences)
+
+    padded_sentences = []
+    for i in range(len(sentences)):
+        sentence = sentences[i]
+        print(sentence)
+        num_padding = sequence_length - len(sentence)
+        new_sentence = sentence + [padding_word] * num_padding
+        padded_sentences.append(new_sentence)
+    return padded_sentences
+
